263a711fa03911c22850c4bf2e1b813590d60670
CCC <CCC@qq.com>
2022-05-31 20:57:02 +0800

    第一次提交

---
+++ src/main/java/com/ccc/test.java
@@ -0,0 +1,10 @@
+package com.ccc;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈");
+        System.out.println("哈哈哈哈1");
+        System.out.println("哈哈哈哈2");
+        System.out.println("哈哈哈哈3");
+    }
+}
