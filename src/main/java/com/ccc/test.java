ecb533091441b7fb65cbe202029d0943e1f28979
CCC <CCC@qq.com>
2022-05-31 23:12:32 +0800

    1

--- src/main/java/com/ccc/test.java
+++ src/main/java/com/ccc/test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("哈哈哈哈");
         System.out.println("哈哈哈哈1");
-        System.out.println("哈哈哈哈2");
-        System.out.println("哈哈哈哈3");
+        System.out.println("ccccccc");
+
     }
 }
